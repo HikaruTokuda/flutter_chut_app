041d24c8547cbe8e4795c81a3a0984777a7f8bea
Tokuda Hikaru <hkrtkd@gmail.com>
2022-01-21 21:18:19 +0900

    ListViewにユーザ情報を表示する処理を追加

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chut_app/model/user.dart';
 
@@ -13,13 +14,13 @@ class _TopPageState extends State<TopPage> {
     User(
       name: '田中',
       uid: 'abc',
-      imagePath: 'https://source.unsplash.com/bIhpiQA009k',
+      imagePath: 'https://source.unsplash.com/random',
       lastMessage: 'こんにちは'
     ),
     User(
         name: '小林',
         uid: 'def',
-        imagePath: 'https://source.unsplash.com/bIhpiQA009k',
+        imagePath: 'https://source.unsplash.com/random',
         lastMessage: 'ありがとう'
     )
   ];
@@ -30,7 +31,34 @@ class _TopPageState extends State<TopPage> {
       appBar: AppBar(
         title: Text('チャットアプリ'),
       ),
-      body: Center(child: Text('Top page')),
+      // ListViewの作成
+      body: ListView.builder(
+        itemCount: userList.length,               // Listの行数を指定
+        itemBuilder: (context, index) {           // 行の作成
+          return Container(
+            height: 70,
+            child: Row(                             // 行の要素を作成
+              children: [                           // 1行に入れ込むウィジェットを指定
+              Padding(    // CircleAvatarに対してパディングを設定
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),             // 左右に同じパディングを設定
+                child: CircleAvatar(                                              // 丸形のアイコン
+                    backgroundImage: NetworkImage(userList[index].imagePath!),    // アイコンの画像をAPIより取得
+                    radius: 30,                                                   // アイコンの大きさ
+                  ),
+              ),
+                Column(                                               // 要素を縦に並べる
+                  mainAxisAlignment: MainAxisAlignment.center,        // 表示の開始位置を指定(縦)
+                  crossAxisAlignment: CrossAxisAlignment.start,       // 表示の開始位置を指定(横)
+                  children: [                                         // ここに入っている要素が縦に並ぶ
+                    Text(userList[index].name!, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                    Text(userList[index].lastMessage!, style: TextStyle(color: Colors.grey),)
+                  ],
+                )
+              ],
+            ),
+          );
+        }
+      ),
     );
   }
 }

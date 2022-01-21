e2fd249a4b44cae56dc4eeb0562fb3f624e9ed89
Tokuda Hikaru <hkrtkd@gmail.com>
2022-01-21 12:28:58 +0900

    ダミーデータ作成

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chut_app/model/user.dart';
 
 class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
@@ -8,6 +9,21 @@ class TopPage extends StatefulWidget {
 }
 
 class _TopPageState extends State<TopPage> {
+  List<User> userList = [
+    User(
+      name: '田中',
+      uid: 'abc',
+      imagePath: 'https://source.unsplash.com/bIhpiQA009k',
+      lastMessage: 'こんにちは'
+    ),
+    User(
+        name: '小林',
+        uid: 'def',
+        imagePath: 'https://source.unsplash.com/bIhpiQA009k',
+        lastMessage: 'ありがとう'
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

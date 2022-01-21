df6fb2cd26d9f42018c37ffbcb6a5517a7a5364f
Tokuda Hikaru <hkrtkd@gmail.com>
2022-01-21 21:24:48 +0900

    トークルーム画面を追加

---
+++ lib/pages/talk_room.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TalkRoom extends StatefulWidget {
+  const TalkRoom({Key? key}) : super(key: key);
+
+  @override
+  _TalkRoomState createState() => _TalkRoomState();
+}
+
+class _TalkRoomState extends State<TalkRoom> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
